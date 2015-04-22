9f5dae126547f020d1df26b9570de250f4e47db5
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-04-21 22:43:50 -0500

    Formatting JS, remove dup jquery ready call

--- js/global.js
+++ js/global.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function($) {
-
-
 	// Toggle mobile menu
-	$(".nav-toggle").on("click", function(){	
+	$(".nav-toggle").on("click", function() {
 		$(this).toggleClass("active");
 		$(".primary-menu").slideToggle();
 		if ($(".search-toggle").hasClass("active")) {
@@ -11,10 +9,8 @@ jQuery(document).ready(function($) {
 		}
 		return false;
 	});
-	
-	
 	// Toggle search container
-	$(".search-toggle").on("click", function(){	
+	$(".search-toggle").on("click", function() {
 		$(this).toggleClass("active");
 		$(".search-container").slideToggle();
 		if ($(".nav-toggle").hasClass("active")) {
@@ -23,72 +19,52 @@ jQuery(document).ready(function($) {
 		}
 		return false;
 	});
-	
-	
 	// Hide mobile menu/search container at resize
 	$(window).resize(function() {
-	
 		if ($(window).width() >= 850) {
 			$(".nav-toggle").removeClass("active");
 		}
-		
 		if ($(window).width() <= 850) {
 			$(".search-toggle").removeClass("active");
 			$('.search-container').hide();
 		}
-	
 	});
-	
-	
 	// Dropdown menus on touch devices
-	$( '.primary-menu li:has(ul)' ).doubleTapToGo();
-	$( '.secondary-menu li:has(ul)' ).doubleTapToGo();
-	
-	
+	$('.primary-menu li:has(ul)').doubleTapToGo();
+	$('.secondary-menu li:has(ul)').doubleTapToGo();
 	// Load Flexslider
-    $(".flexslider").flexslider({
-        animation: "slide",
-        controlNav: false,
-        prevText: "",
-        nextText: "",
-        smoothHeight: true   
-    });
-	
-	
-	// smooth scroll to the top	
-	jQuery(document).ready(function($){
-	    $('.to-the-top').click(function(){
-	        $("html, body").animate({ scrollTop: 0 }, 500);
-	        return false;
-	    });
+	$(".flexslider").flexslider({
+		animation: "slide",
+		controlNav: false,
+		prevText: "",
+		nextText: "",
+		smoothHeight: true
 	});
-	
-	
+	// smooth scroll to the top	
+	$('.to-the-top').click(function() {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 500);
+		return false;
+	});
 	// resize videos after container
-	var vidSelector = ".post iframe, .post object, .post video";	
+	var vidSelector = ".post iframe, .post object, .post video";
 	var resizeVideo = function(sSel) {
-		$( sSel ).each(function() {
-			var $video = $(this),
-				$container = $video.parent(),
-				iTargetWidth = $container.width();
-
-			if ( !$video.attr("data-origwidth") ) {
-				$video.attr("data-origwidth", $video.attr("width"));
-				$video.attr("data-origheight", $video.attr("height"));
-			}
-
-			var ratio = iTargetWidth / $video.attr("data-origwidth");
-
-			$video.css("width", iTargetWidth + "px");
-			$video.css("height", ( $video.attr("data-origheight") * ratio ) + "px");
-		});
-	};
-
+			$(sSel).each(function() {
+				var $video = $(this),
+					$container = $video.parent(),
+					iTargetWidth = $container.width();
+				if (!$video.attr("data-origwidth")) {
+					$video.attr("data-origwidth", $video.attr("width"));
+					$video.attr("data-origheight", $video.attr("height"));
+				}
+				var ratio = iTargetWidth / $video.attr("data-origwidth");
+				$video.css("width", iTargetWidth + "px");
+				$video.css("height", ($video.attr("data-origheight") * ratio) + "px");
+			});
+		};
 	resizeVideo(vidSelector);
-
 	$(window).resize(function() {
 		resizeVideo(vidSelector);
 	});
-	
-
 });
