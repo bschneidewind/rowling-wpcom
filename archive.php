d8c1db015f6dfccafddb7ae7fa55799c2a42e3dd
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-04-23 02:06:23 -0500

    Finalize archives template

--- archive.php
+++ archive.php
@@ -7,46 +7,35 @@
  
 get_header(); ?>
 
-<div class="wrapper section-inner clearfix">
-	<div class="content">
-		<div class="page-title">
-			<?php the_archive_title( '<h4>', '</h4>' ); ?>
-				
-				<?php $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-					if ( '1' < $wp_query->max_num_pages ) : ?>
-				
-					<p><?php esc_html_e( 'Page', 'rowling' ); echo ' ' . $paged . '<span class="sep">/</span>' . $wp_query->max_num_pages; ?></p>
-					
-					<div class="clear"></div>
-				
-				<?php endif; ?>
-				
-				<div class="clear"></div>
-				
+	<div class="wrapper section-inner clearfix">
+		<div class="content">
+			<div class="page-title clearfix">
+				<?php the_archive_title( '<h4>', '</h4>' ); ?>
+
+				<?php if( $wp_query->max_num_pages > 1 ) :
+					$paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
+					$maxpage = $wp_query->max_num_pages;
+					printf( '<p>Page %u<span class="sep">/</span>%s</p>', intval( $paged ), intval( $maxpage ) );
+					endif; ?>
+
 			</div> <!-- /page-title -->
-			
-			<?php if ( have_posts() ) : ?>
-			
-				<?php rewind_posts(); ?>
-					
+
+			<?php if ( have_posts() ) : rewind_posts(); ?>
+
 				<div class="posts" id="posts">
-					
 					<?php while ( have_posts() ) : the_post(); ?>
-								
 						<?php get_template_part( 'content', get_post_format() ); ?>
-						
 					<?php endwhile; ?>
-									
 				</div> <!-- /posts -->
-				
+
 				<?php rowling_archive_navigation(); ?>
-						
+
 			<?php endif; ?>
-		
+
 		</div> <!-- /content -->
-		
+
 		<?php get_sidebar(); ?>
-	
+
 	</div> <!-- /wrapper.section-inner -->
 
 <?php get_footer();
