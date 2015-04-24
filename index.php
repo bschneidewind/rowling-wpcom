0051b510dedfe1f816313b411d8b5db95b5937aa
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-04-24 01:25:46 -0500

    Finalize index template

--- index.php
+++ index.php
@@ -5,48 +5,38 @@
  * @package Rowling
  */
  
- get_header(); ?>
+get_header(); ?>
+ 
+	<div class="wrapper section-inner clearfix">
+		<div class="content">
+			<div class="page-title clearfix">
+				<?php the_title( '<h4>', '</h4>' );  ?>
 
-<div class="wrapper section-inner clearfix">
-			
-	<div class="content">				
-																							                    
-		<?php if ( have_posts() ) : ?>
-		
-			<?php $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-			
-			if ( '1' < $paged ) : ?>
-			
-				<div class="page-title">
-					
-					<h4>Archive</h4>
-				
-					<p><?php esc_html_e( 'Page', 'rowling' ); echo ' ' . $paged . '<span class="sep">/</span>' . $wp_query->max_num_pages; ?></p>
-					
-					<div class="clear"></div>
-					
-				</div> <!-- /page-title -->
-							
+				<?php if( $wp_query->max_num_pages > 1 ) :
+					$paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
+					$maxpage = $wp_query->max_num_pages;
+					printf( __( '<p>Page %u<span class="sep">/</span>%s</p>' ), intval( $paged ), intval( $maxpage ) );
+				endif; ?>
+			</div> <!-- /page-title -->
+
+			<?php if ( have_posts() ) : ?>
+
+				<div class="posts" id="posts">
+	
+					<?php while (have_posts()) : the_post(); ?>
+						<?php get_template_part( 'content', get_post_format() ); ?>
+					<?php endwhile; ?>
+	
+				</div> <!-- /posts -->
+	
+				<?php rowling_archive_navigation(); ?>
+
 			<?php endif; ?>
-		
-			<div class="posts" id="posts">
-					
-		    	<?php while (have_posts()) : the_post(); ?>
-		    	
-		    		<?php get_template_part( 'content', get_post_format() ); ?>
-		    			        		            
-		        <?php endwhile; ?>
-	        	                    			
-			</div> <!-- /posts -->
-		
-			<?php rowling_archive_navigation(); ?>
-			
-		<?php endif; ?>
-		
-	</div> <!-- /content -->
-	
-	<?php get_sidebar(); ?>
-	
-</div> <!-- /wrapper.section-inner -->
-	              	        
+
+		</div> <!-- /content -->
+
+		<?php get_sidebar(); ?>
+
+	</div> <!-- /wrapper.section-inner -->
+
 <?php get_footer();
