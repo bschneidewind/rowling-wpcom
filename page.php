e1cc042969dcb7a9bb88463e9d203e4b3f747abe
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-04-25 01:33:12 -0500

    Cleanup/finalize page template

--- page.php
+++ page.php
@@ -1,55 +1,47 @@
 <?php
-/**
- * The default page template
- *
- * @package Rowling
- */
-
+	/**
+		* The default page template
+		*
+		* @package Rowling
+	*/
 get_header(); ?>
 
-<div class="wrapper section-inner clearfix">
-	<div class="content">
+	<div class="wrapper section-inner clearfix">
+		<div class="content">
 
-	<?php while ( have_posts() ) : the_post(); ?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class( 'post single single-post' ); ?>>
-			<div class="post-header">
-				<h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			</div> <!-- /post-header -->
+			<?php while ( have_posts() ) : the_post(); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class( 'post single single-post' ); ?>>
+				<div class="post-header">
+					<h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				</div> <!-- /post-header -->
 
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="post-image">
-				<?php printf( '<a href="%s" title="%s">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), the_post_thumbnail( 'post-image' ) ); ?>
+				<?php if ( has_post_thumbnail() ) : ?>
+				<div class="post-image">
+					<?php printf( '<a href="%s" title="%s">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), the_post_thumbnail( 'post-image' ) ); ?>
+					<?php $get_description = get_post( get_post_thumbnail_id() )->post_excerpt;
+						if( !empty( $get_description ) ) {
+							printf( '<p class="post-image-caption"><span class="fa fw fa-camera"></span>%s</p>', esc_html( $get_description ) );
+						} ?>
 
-				<?php if ( $id = get_post_thumbnail_id() ) {
-					$post = get_post( $id );
-					$caption = $post->post_excerpt;
-				
-					// escape the caption
-					$caption = array_map( 'esc_html', $caption );
-				
-					if ( ! empty ( $caption ) )
-						echo '<p class="post-image-caption"><span class="fa fw fa-camera"></span>' . $caption . '</p>';
-				} ?>
+				</div> <!-- /post-image -->
+				<?php endif; ?>
 
-			</div> <!-- /post-image -->
-		<?php endif; ?>
+				<div class="post-inner">
+					<div class="post-content">
+						<?php the_content(); ?>
+						<?php edit_post_link( __( 'Edit', 'rowling' ), '<p class="page-edit-link"><span class="fa fw fa-wrench"></span>', '</p>' ); ?>
+					</div> <!-- /post-content -->
+				</div> <!-- /post-inner -->
 
-			<div class="post-inner">
-				<div class="post-content">
-					<?php the_content(); ?>
-					<?php edit_post_link( __( 'Edit', 'rowling' ), '<p class="page-edit-link"><span class="fa fw fa-wrench"></span>', '</p>' ); ?>
-				</div> <!-- /post-content -->
-			</div> <!-- /post-inner -->
+				<?php comments_template( '', true ); ?>
 
-			<?php comments_template( '', true ); ?>
+			</div> <!-- /post -->
+			<?php endwhile; ?>
 
-		</div> <!-- /post -->
-	<?php endwhile; ?>
+		</div> <!-- /content -->
 
-	</div> <!-- /content -->
+		<?php get_sidebar(); ?>
 
-	<?php get_sidebar(); ?>
-
-</div> <!-- /wrapper.section-inner -->
+	</div> <!-- /wrapper.section-inner -->
 
 <?php get_footer();
