3bacd9b32570e68d6a66486d3c7f23be5129478f
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-04-26 06:19:22 -0500

    Remove ID's on search

--- searchform.php
+++ searchform.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-<form role="search" method="get" class="search-form" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="search" class="search-field" placeholder="<?php esc_html_e( 'Search form', 'rowling' ); ?>" name="s" id="default_search" /> 
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<input type="search" class="search-field" placeholder="<?php esc_html_e( 'Search form', 'rowling' ); ?>" name="s" /> 
 	<button type="submit" class="search-button"><span class="fa fw fa-search"></span></button>
 </form>
