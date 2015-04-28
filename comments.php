33e5dbe51ed1ad4358ef5600ba8b6ba1f1eead2b
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-04-28 01:59:18 -0500

    Spacing fix

--- comments.php
+++ comments.php
@@ -20,26 +20,26 @@ $comments_args = array(
 	
 	'comment_field' 		=> 
 		'<p class="comment-form-comment">
-			<label for="comment">' . __('Comment','rowling') . ( $req ? '<span class="required">*</span>' : '' ) . '</label>
+			<label for="comment">' . __( 'Comment','rowling' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label>
 			<textarea id="comment" name="comment" cols="45" rows="6" required></textarea>
 		</p>',
 	
 	'fields' => apply_filters( 'comment_form_default_fields', array(
 		'author' =>
 			'<p class="comment-form-author">
-				<label for="author">' . __('Name','rowling') . ( $req ? '<span class="required">*</span>' : '' ) . '</label> 
+				<label for="author">' . __( 'Name','rowling' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label> 
 				<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" />
 			</p>',
 		
 		'email' =>
 			'<p class="comment-form-email">
-				<label for="email">' . __('Email','rowling') . ( $req ? '<span class="required">*</span>' : '' ) . '</label> 
+				<label for="email">' . __( 'Email','rowling' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label> 
 				<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" />
 			</p>',
 		
 		'url' =>
 			'<p class="comment-form-url">
-				<label for="url">' . __('Website','rowling') . '</label>
+				<label for="url">' . __( 'Website','rowling' ) . '</label>
 				<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" />
 			</p>')
 	),
