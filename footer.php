f639f3d7b84daa5c090ba9a0e5e9acc9bda4b6ae
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-04-28 17:45:40 -0500

    Footer update

--- footer.php
+++ footer.php
@@ -11,9 +11,8 @@
 			<div class="section-inner">
 				<a href="#site-header" class="to-the-top" title="<?php esc_attr_e( 'To the top', 'rowling' ); ?>"><span class="fa fw fa-arrow-up"></span></a>
 				<div class="copyright">
-					<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'rowling' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'rowling' ), 'WordPress' ); ?></a>
+					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'rowling' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'rowling' ), 'WordPress' ); ?></a>
 				</div> <!-- /copyright -->
-
 				<div class="attribution">
 					<?php printf( __( 'Theme: %1$s by %2$s.', 'rowling' ), 'Rowling', '<a href="http://www.andersnoren.se" rel="designer">Anders Nor&eacute;n</a>' ); ?>
 				</div> <!-- /attribution -->
