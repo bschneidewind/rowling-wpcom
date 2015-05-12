b5715564260e3dd63c303ca917010a634b9743c7
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-05-12 03:15:21 -0500

    Default sidebar widgets and styling

--- sidebar.php
+++ sidebar.php
@@ -4,10 +4,13 @@
  *
  * @package Rowling
  */
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
-?>
+if ( ! is_active_sidebar( 'sidebar-1' ) ) : ?>
+	<div class="sidebar">
+		<?php the_widget( 'WP_Widget_Search' ); ?>
+		<?php the_widget( 'WP_Widget_Categories' ); ?>
+		<?php the_widget( 'WP_Widget_Recent_Comments' ); ?>
+	</div>
+<?php endif; ?>
 
 <div class="sidebar" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
