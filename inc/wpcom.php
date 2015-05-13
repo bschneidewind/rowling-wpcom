47d74f5cf889e5ed8dd72187251fa38fce7dd790
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-05-13 01:29:15 -0500

    theme colors addition

--- inc/wpcom.php
+++ inc/wpcom.php
@@ -1,14 +1,24 @@
 <?php
 
-if ( ! isset( $themecolors ) ) {
-	$themecolors = array(
-		'bg'     => 'ffffff',
-		'text'   => '000000',
-		'link'   => '667755',
-		'border' => 'cccccc',
-		'url'    => '99aa88',
-	);
+/**
+ * Adds support for wp.com-specific theme functions.
+ *
+ * @global array $themecolors
+ */
+function _s_wpcom_setup() {
+	global $themecolors;
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		$themecolors = array(
+			'bg'     => 'ffffff',
+			'text'   => '000000',
+			'link'   => '667755',
+			'border' => 'cccccc',
+			'url'    => '99aa88',
+		);
+	}
 }
+add_action( 'after_setup_theme', '_s_wpcom_setup' );
 
 /*
  * De-queue Google fonts if custom fonts are being used instead
