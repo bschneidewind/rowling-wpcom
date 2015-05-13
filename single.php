9cf22d559fcfb01f1af2c6d39bade5344b73c0a9
bschneidewind <bschneidewind@REMLMACLWDKQ2.dfs.com>
2015-05-13 01:28:59 -0500

    Prev/next post navigation

--- single.php
+++ single.php
@@ -11,8 +11,16 @@ get_header(); ?>
 	<div class="content">
 
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-				get_template_part( 'content', get_post_format() );
-				comments_template( '', true );
+				get_template_part( 'content', get_post_format() ); ?>
+				<div class="single-post-nav clearfix">
+					<div class="fleft">
+						<?php previous_post_link(); ?>
+					</div>
+					<div class="fright">
+						<?php next_post_link(); ?>
+					</div>
+				</div>
+				<?php comments_template( '', true );
 			endwhile; endif; ?>
 	</div> <!-- /content -->
 	<?php get_sidebar(); ?>
